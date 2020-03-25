5db39c422d71d971b93c01a6322d2b8362049b97
zhangml123 <s15532292192@163.com>
2020-03-25 09:02:17 +0800

    update files

--- app/controllers/discourse_faucet/faucet_controller.rb
+++ app/controllers/discourse_faucet/faucet_controller.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 module DiscourseFaucet
   class FaucetController < ApplicationController
-
     def index
+
+
+
+
+      	
       render_json_dump("PlatON NewBaleyworld testnet faucet")
     end
-
   end
 end
--- plugin.rb
+++ plugin.rb
@@ -4,6 +4,6 @@
 # authors: null
 # url: https://github.com/zhangml123/discourse_faucet_plugin
 
-
+puts "faucet plugin"
 load File.expand_path('../lib/engine.rb', __FILE__)
 
